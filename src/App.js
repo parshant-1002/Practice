02711f53ba19e0a8c78ce0211998e14ef4accde0
parshant-1002 <parshant.sharma@chicmic.co.in>
2023-02-23 18:48:24 +0530

    crudapp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home';
+import Show from './components/show';
+import SideBar from './components/sideBar';
+
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+
+    <>
+      <table className="table m-4">
+        <tc  className=" m-2">
+          <td>
+
+            <div className=" justify-content-start ">
+
+              <SideBar />
+            </div>
+          </td>
+
+        </tc>
+        <tc>
+          <td>
+            <div className="d-flex justify-content-end p-5 w-100">
+
+              <Routes>
+                <Route exact path="/" element={<Home />}> </Route>
+                <Route path="/show" element={<Show />}> </Route>
+              </Routes>
+            </div>
+
+          </td>
+
+        </tc>
+
+
+
+
+      </table>
+
+    </>
+
+
+
+
   );
 }
 
