3a41e508dc81a943eec9d9c708e8d77403e24c21
parshant-1002 <parshant.sharma@chicmic.co.in>
2023-06-06 12:19:56 +0530

    initial Commit

---
+++ goblin-sax/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import ConnectWallet from './Component';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Connect Wallet
+        <ConnectWallet/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
