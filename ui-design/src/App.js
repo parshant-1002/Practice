282343e368cd03a0bf233e5101161542d715fd88
parshant-1002 <parshant.sharama@chicmic.co.in>
2023-09-26 14:32:10 +0530

    designing practice

---
+++ ui-design/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My design</h1>
+      <div className="main-div text-danger ">fsfsdf</div>
+    </div>
+  );
+}
+
+export default App;
